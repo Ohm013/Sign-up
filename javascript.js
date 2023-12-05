7043549d04a08d8b3ed85223a3a0ebc9639af78d
Ohm Patel <60417541+Ohm013@users.noreply.github.com>
2023-12-05 12:57:00 -0600

    made logic for matching confirm password with event listeners and function with if statements

--- javascript.js
+++ javascript.js
@@ -1,18 +1,24 @@
- 
- 
- const password = document.querySelector(".password");
- const confirmPass = document.querySelector(".confirmPass");
- const matchPass = document.querySelector(".matchPass");
+const password = document.querySelector("#password");
+const confirmPass = document.querySelector("#confirmPass");
+const matchPass = document.querySelector("#matchPass");
 
- function passwordCheck () {
-    if (confirmPass.value !== password.value)  {
 
-        matchPass.textContent = "Password does not match"
-    } else {
-        matchPass.textContent = ""; 
+function passwordCheck() {
+    if (confirmPass.value == password.value)  {
+        matchPass.textContent = "Matching"
+        confirmPass.style.borderColor = " rgb(118, 225, 118)"
+    } else if (password.value !== confirmPass.value) {
+        matchPass.textContent= " Not Matching"; 
+        confirmPass.style.borderColor = "red"
     }
- }
+};
 
- passwordCheck(); 
 
- // figure out password check
+password.addEventListener('keyup', () => {
+    if (confirmPass.value.length != 0) {
+        passwordCheck();
+
+    }
+});
+
+confirmPass.addEventListener('keyup', passwordCheck);
